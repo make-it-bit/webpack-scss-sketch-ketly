14a8ccddaab1671058898e04b7973c11c617d89e
ketlyiouriev <ketly.iouriev@ut.ee>
2023-06-21 20:53:30 +0300

    both forms are done

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,16 @@
 import '../scss/index.scss';
+import '../scss/reset.scss';
 import '../../node_modules/bootstrap/scss/bootstrap-grid.scss';
+
+document.getElementById('nav')?.addEventListener('change', () => {
+  let checkedNavHeading = document.querySelector('input[name="heading"]:checked')?.value;
+  console.log('checkedNavHeading: ', checkedNavHeading);
+  if (checkedNavHeading === 'calculate') {
+    document.getElementById('generator-2').style.display = 'none';
+    document.getElementById('generator-1').style.display = 'block';
+  } else if (checkedNavHeading === 'enter') {
+    document.getElementById('generator-1').style.display = 'none';
+    document.getElementById('generator-2').style.display = 'block';
+  }
+  return;
+});
